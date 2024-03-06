5b60dac5727aeacecfe9d8dac0c8de646d4afd03
maria.akulova <maria.akulova.dragon@gmail.com>
2024-03-06 13:19:03 +0100

    feat: makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -137,8 +137,12 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const words = new Array(100);
+  Object.entries(lettersObject).forEach(([key, value]) =>
+    value.forEach((index) => words.fill(key, index, index + 1))
+  );
+  return words.join('');
 }
 
 /**
